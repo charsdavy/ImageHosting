00e4bfbce479bcaf32a570ca175986e74200a660
邓伟 <dengwei@myzaker.com>
2016-09-01 14:05:07 +0800

    Update Version Number

--- ImageHosting/Controller/Window/IHAboutWindowController.m
+++ ImageHosting/Controller/Window/IHAboutWindowController.m
@@ -8,8 +8,8 @@
 
 #import "IHAboutWindowController.h"
 
-#define VERSION @"1.0.4"
-#define BUILD   @"5"
+#define VERSION @"1.0.5"
+#define BUILD   @"6"
 
 @interface IHAboutWindowController ()
 
@@ -31,9 +31,9 @@
 - (void)windowDidLoad
 {
     [super windowDidLoad];
-
+    
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-
+    
     self.versionTextField.stringValue = [self version];
 }
 
